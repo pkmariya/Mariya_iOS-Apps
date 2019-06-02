b39f4f831911ade4476993a188675057771c2e8d
Mariya <pkmariya@yahoo.com>
2019-06-02 06:04:46 +0530

    Save data persistence using NSCoder.

---
+++ Tableviews/TableviewMVC/TableviewMVC/Data Model/NamesClass.swift
@@ -0,0 +1,14 @@
+//
+//  NamesClass.swift
+//  TableviewMVC
+//
+//  Created by Mariya Arul Raj on 01/06/19.
+//  Copyright © 2019 Mariya Arul Raj. All rights reserved.
+//
+
+import Foundation
+
+class NamesClass: Codable {
+    var title: String = ""
+    var done: Bool = false
+}
